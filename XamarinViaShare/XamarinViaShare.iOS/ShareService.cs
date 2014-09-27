7a4a0b39006a4004b19c4a28bb8bd7bd24d74b57
sharker86 <sharker86@gmail.com>
2014-09-27 09:01:20 +0200

    a

--- XamarinViaShare/XamarinViaShare.iOS/ShareService.cs
+++ XamarinViaShare/XamarinViaShare.iOS/ShareService.cs
@@ -14,7 +14,49 @@ namespace XamarinViaShare.iOS
     {
         public void Share(string title, string details, string url)
         {
+            //NSLog(@"shareButton pressed");
 
+            //NSString *texttoshare = _txt; //this is your text string to share
+            //UIImage *imagetoshare = _img; //this is your image to share
+            //NSArray *activityItems = @[texttoshare, imagetoshare];    
+            //UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+            //activityVC.excludedActivityTypes = @[UIActivityTypeAssignToContact, UIActivityTypePrint];
+            //[self presentViewController:activityVC animated:TRUE completion:nil];
+
+            //func shareTextImageAndURL(#sharingText: String?, sharingImage: UIImage?, sharingURL: NSURL?) {
+            //var sharingItems = [AnyObject]()
+
+            //if let text = sharingText {
+            //sharingItems.append(text)
+            //}
+            //if let image = sharingImage {
+            //sharingItems.append(image)
+            //}
+            //if let url = sharingURL {
+            //sharingItems.append(url)
+            //}
+
+            //let activityViewController = UIActivityViewController(activityItems: sharingItems, applicationActivities: nil)
+            //self.presentViewController(activityViewController, animated: true, completion: nil)
+
+            //- (void)shareText:(NSString *)text andImage:(UIImage *)image andUrl:(NSURL *)url
+            //{
+            //NSMutableArray *sharingItems = [NSMutableArray new];
+
+            //if (text) {
+            //[sharingItems addObject:text];
+            //}
+            //if (image) {
+            //[sharingItems addObject:image];
+            //}
+            //if (url) {
+            //[sharingItems addObject:url];
+            //}
+
+            //UIActivityViewController *activityController = [[UIActivityViewController alloc] initWithActivityItems:sharingItems applicationActivities:nil];
+            //[self presentViewController:activityController animated:YES completion:nil];
+}
+}
         }
     }
 }
