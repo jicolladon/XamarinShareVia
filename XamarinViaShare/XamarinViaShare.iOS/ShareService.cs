c63c520bb479189ea3b2f8c99a7405f38006fe3c
sharker86 <joan.isaac.collado@gmail.com>
2014-09-30 10:04:43 +0200

    <

--- XamarinViaShare/XamarinViaShare.iOS/ShareService.cs
+++ XamarinViaShare/XamarinViaShare.iOS/ShareService.cs
@@ -15,9 +15,6 @@ using MonoTouch.CoreFoundation;
 using MonoTouch.Social;
 using MonoTouch.Foundation;
 
-
-
-
 [assembly: Dependency(typeof(XamarinViaShare.iOS.ShareService))]
 namespace XamarinViaShare.iOS
 {
@@ -26,12 +23,10 @@ namespace XamarinViaShare.iOS
 
         public void Share(string title, string details, string url)
         {
-            Tweet();
-            return;
+
             //NSLog(@"shareButton pressed");
-            NSArray textToShare = NSArray.FromObjects(details);
             NSArray textToShare2 = new NSArray();
-            var activityItems = new NSObject[] { new NSString(details )};
+            var activityItems = new NSObject[] { new NSString(title), new NSString(details), new NSUrl(url) };
             
             var excludedActivityTypes = new NSString[] 
             {
@@ -42,77 +37,9 @@ namespace XamarinViaShare.iOS
             var homeViewController = UIApplication.SharedApplication.Windows[0].RootViewController;
             var activityViewController = new UIActivityViewController(activityItems, null);
 
-            
-            
-            activityViewController.ExcludedActivityTypes = excludedActivityTypes;
-            //var navigationController = new UINavigationController(homeViewController);
-            
             homeViewController.PresentViewController(activityViewController, true, null);
-            //this is your text string to share
-            //UIImage *imagetoshare = _img; //this is your image to share
-            //NSArray *activityItems = @[texttoshare, imagetoshare];    
-            //UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
-            //activityVC.excludedActivityTypes = @[UIActivityTypeAssignToContact, UIActivityTypePrint];
-            //[self presentViewController:activityVC animated:TRUE completion:nil];
-
-            //func shareTextImageAndURL(#sharingText: String?, sharingImage: UIImage?, sharingURL: NSURL?) {
-            //var sharingItems = [AnyObject]()
-
-            //if let text = sharingText {
-            //sharingItems.append(text)
-            //}
-            //if let image = sharingImage {
-            //sharingItems.append(image)
-            //}
-            //if let url = sharingURL {
-            //sharingItems.append(url)
-            //}
-
-            //let activityViewController = UIActivityViewController(activityItems: sharingItems, applicationActivities: nil)
-            //self.presentViewController(activityViewController, animated: true, completion: nil)
-
-            //- (void)shareText:(NSString *)text andImage:(UIImage *)image andUrl:(NSURL *)url
-            //{
-            //NSMutableArray *sharingItems = [NSMutableArray new];
-
-            //if (text) {
-            //[sharingItems addObject:text];
-            //}
-            //if (image) {
-            //[sharingItems addObject:image];
-            //}
-            //if (url) {
-            //[sharingItems addObject:url];
-            //}
-
-            //UIActivityViewController activityController = [[UIActivityViewController alloc] initWithActivityItems:sharingItems applicationActivities:nil];
-            //[self presentViewController:activityController animated:YES completion:nil];
+          
         }
-
-
-        //void Share2(string title, string details, string url)
-        //{
-        //    UIActivityViewController activityViewController = new UIActivityViewController(new NSObject[] {
-        //        new NSString(details),
-        //    }, null);
-
-        //    // Set a completion handler to handle what the UIActivityViewController returns
-        //    activityViewController.SetCompletionHandler((activityType, completed, returnedItems, error) =>
-        //    {
-        //        if (returnedItems == null
-        //           || returnedItems.Length == 0)
-        //            return;
-
-        //        NSExtensionItem extensionItem = returnedItems[0];
-        //        NSItemProvider imageItemProvider = extensionItem.Attachments[0];
-
-        //        if (!imageItemProvider.HasItemConformingTo(UTType.Text))
-        //            return;
-        //    });
-        //    var homeViewController = UIApplication.SharedApplication.Windows[0].RootViewController;
-
-        //    homeViewController.PresentViewController(activityViewController, true, null);
-        //}
 
         public void OpenMail(string email, string subject, string body)
         {
@@ -132,33 +59,28 @@ namespace XamarinViaShare.iOS
 
         public void Tweet()
         {
+            //works
             var homeViewController = UIApplication.SharedApplication.Windows[0].RootViewController;
 
-            if (!TWTweetComposeViewController.CanSendTweet)
-            {
-                var tweet = new TWTweetComposeViewController();
-                tweet.SetInitialText("Tweeting from my iOS app");
+            var tweet = new TWTweetComposeViewController();
+
+            tweet.SetInitialText("Tweeting from my iOS app");
                 
-                tweet.AddUrl (new NSUrl("http://xamarin.com"));
-                tweet.SetCompletionHandler((TWTweetComposeViewControllerResult r) =>
+            tweet.AddUrl (new NSUrl("http://xamarin.com"));
+            tweet.SetCompletionHandler((TWTweetComposeViewControllerResult r) =>
+            {
+                homeViewController.DismissViewController(true,null); // hides the tweet
+                if (r == TWTweetComposeViewControllerResult.Cancelled)
                 {
-                    homeViewController.DismissViewController(true,null); // hides the tweet
-                    if (r == TWTweetComposeViewControllerResult.Cancelled)
-                    {
-                        // user cancelled the tweet
-                    }
-                    else
-                    {
-                        return;
-                    }
-                });
-                
-                homeViewController.PresentViewController(tweet, true,null);
-            }
-            else
-            {
-                // Show a message: Twitter may not be configured in Settings
-            }
+                    // user cancelled the tweet
+                }
+                else
+                {
+                    return;
+                }
+            });
+            homeViewController.PresentViewController(tweet, true,null);
+
         }
     }
 }
